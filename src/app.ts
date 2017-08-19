06356f0eec453af6393eed9b089611de2520bf31
Cindy Dallaire <c.dallaire93@gmail.com>
2017-08-19 14:18:13 -0400

    Add username example

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,12 @@ class Greeter {
 
     constructor(element: HTMLElement) {
         this.element = element;
-        this.element.innerHTML += "The time is: ";
+        this.element.innerHTML += "You are : ";
         this.span = document.createElement("span");
         this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+        this.span.innerText = VSS.getWebContext().user.name;
     }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
 }
 
 const el = document.getElementById("content");
 const greeter = new Greeter(el);
-greeter.start();
