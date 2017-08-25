4cbb6b20f403e38243d0ad45ca139866a5d84301
Cindy Dallaire <c.dallaire93@gmail.com>
2017-08-24 22:19:39 -0400

    Working example with reading effort

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
  /// <reference types="vss-web-extension-sdk" />
+import RestClient = require("TFS/WorkItemTracking/RestClient");
+import Contracts = require("TFS/WorkItemTracking/Contracts");
 
 class Greeter {
     element: HTMLElement;
@@ -16,3 +18,12 @@ class Greeter {
 
 const el = document.getElementById("content");
 const greeter = new Greeter(el);
+
+// Get an instance of the client
+let client = RestClient.getClient();
+
+client.getWorkItem(1).then(function(wi)
+{
+    console.log("Effort is : " + wi.fields["Microsoft.VSTS.Scheduling.Effort"]);
+}
+);
