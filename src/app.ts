405325c9e40533841d9231917667ac26d6adeafe
Cindy Dallaire <c.dallaire93@gmail.com>
2017-08-29 17:51:23 -0400

    Add missing files

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,14 @@
  /// <reference types="vss-web-extension-sdk" />
 import RestClient = require("TFS/WorkItemTracking/RestClient");
 import Contracts = require("TFS/WorkItemTracking/Contracts");
+import Extension_Data = require("VSS/SDK/Services/ExtensionData");
+import Q = require("q");
+import Players = require("./player");
+import Moderators = require("./moderator");
 
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "You are : ";
-        this.span = document.createElement("span");
-        this.element.appendChild(this.span);
-        this.span.innerText = VSS.getWebContext().user.name;
-    }
+if (VSS.getContribution().type === "ms.vss-web.hub") {
+    new Players.Player(document.getElementById("content"));
 }
-
-const el = document.getElementById("content");
-const greeter = new Greeter(el);
-
-// Get an instance of the client
-let client = RestClient.getClient();
-
-client.getWorkItem(1).then(function(wi)
-{
-    console.log("Effort is : " + wi.fields["Microsoft.VSTS.Scheduling.Effort"]);
+else {
+    new Moderators.Moderator(document.getElementById("content"));
 }
-);
